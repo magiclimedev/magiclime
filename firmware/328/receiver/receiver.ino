c570a36def72883c50d6ddf6cf768a30d7fa9be0
MarlynAnderson <marlyn@thingbits.com>
2022-09-14 16:02:03 -0500

    Update receiver.ino

--- firmware/328/receiver/receiver.ino
+++ firmware/328/receiver/receiver.ino
@@ -222,7 +222,8 @@ void ProcessRXbuf() { //rxBuf[0-3]
           switch (protocol) { 
             case '1': {   pNum=5; // 5 pairs in this protocol
               //compose rss pair and first-half of data pairs, 
-              strcpy(jp[0],"{\"source\":\"rx\",\"rss\":"); char chr[10]; itoa(rxRSS[ptr],chr,10);
+              strcpy(jp[0],"\"rss\":\"");
+                char chr[10]; itoa(rxRSS[ptr],chr,10);
               strcat(jp[0],chr); strcat(jp[0],","); //integer
               strcpy(jp[1],"\"id\":\"");     //data is string (6) , 20-5=15 left for data object
               strcpy(jp[2],"\"type\":\"");   //data is string, 20-7, the verbose one 
@@ -290,7 +291,7 @@ boolean CheckRXbuf() {//********* get RF Messages
 
 //*****************************************
 void json_PRINT(char jsn[][20], byte pNum) {
-  //Serial.print(F("DATA: "));
+  Serial.print(F("{\"source\":\"tx\","));
   for (byte pn=0;pn<pNum;pn++) { //Pair Number
     Serial.print( jsn[pn] ); }
   Serial.println(""); Serial.flush();  
